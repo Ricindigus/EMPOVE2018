30f5a306c0244a428d04adfe9274a075df3b3f11
denis morales <ricindigus@gmail.com>
2018-10-09 11:44:39 -0500

    archivos pregunta completos

---
+++ app/src/main/java/com/example/ricindigus/empove2018/fragments/modulo7/FragmentP706P709.java
@@ -0,0 +1,30 @@
+package com.example.ricindigus.empove2018.fragments.modulo7;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.ricindigus.empove2018.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FragmentP706P709 extends Fragment {
+
+
+    public FragmentP706P709() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_p706_p709, container, false);
+    }
+
+}
