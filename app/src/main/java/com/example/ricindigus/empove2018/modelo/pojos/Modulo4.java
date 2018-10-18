346a4672070e39d0568c58a767de4868dda976c0
cesarfisi66 <cesar.fisi.sistemas.66@gmail.com>
2018-10-18 15:40:51 -0500

    commit cesar 11 - pojos - modulos: 4 y 5

--- app/src/main/java/com/example/ricindigus/empove2018/modelo/pojos/Modulo4.java
+++ app/src/main/java/com/example/ricindigus/empove2018/modelo/pojos/Modulo4.java
@@ -70,6 +70,791 @@ public class Modulo4 {
     private String c4_p408_6;
     private String c4_p408_7;
     private String c4_p408_8;
+
+    public int get_id() {
+        return _id;
+    }
+
+    public void set_id(int _id) {
+        this._id = _id;
+    }
+
+    public String getIdInformante() {
+        return idInformante;
+    }
+
+    public void setIdInformante(String idInformante) {
+        this.idInformante = idInformante;
+    }
+
+    public String getIdHogar() {
+        return idHogar;
+    }
+
+    public void setIdHogar(String idHogar) {
+        this.idHogar = idHogar;
+    }
+
+    public String getIdVivienda() {
+        return idVivienda;
+    }
+
+    public void setIdVivienda(String idVivienda) {
+        this.idVivienda = idVivienda;
+    }
+
+    public String getC4_p401_1() {
+        return c4_p401_1;
+    }
+
+    public void setC4_p401_1(String c4_p401_1) {
+        this.c4_p401_1 = c4_p401_1;
+    }
+
+    public String getC4_p401_2() {
+        return c4_p401_2;
+    }
+
+    public void setC4_p401_2(String c4_p401_2) {
+        this.c4_p401_2 = c4_p401_2;
+    }
+
+    public String getC4_p401_3() {
+        return c4_p401_3;
+    }
+
+    public void setC4_p401_3(String c4_p401_3) {
+        this.c4_p401_3 = c4_p401_3;
+    }
+
+    public String getC4_p401_4() {
+        return c4_p401_4;
+    }
+
+    public void setC4_p401_4(String c4_p401_4) {
+        this.c4_p401_4 = c4_p401_4;
+    }
+
+    public String getC4_p401_5() {
+        return c4_p401_5;
+    }
+
+    public void setC4_p401_5(String c4_p401_5) {
+        this.c4_p401_5 = c4_p401_5;
+    }
+
+    public String getC4_p401_o() {
+        return c4_p401_o;
+    }
+
+    public void setC4_p401_o(String c4_p401_o) {
+        this.c4_p401_o = c4_p401_o;
+    }
+
+    public String getC4_p402() {
+        return c4_p402;
+    }
+
+    public void setC4_p402(String c4_p402) {
+        this.c4_p402 = c4_p402;
+    }
+
+    public String getC4_p403_1() {
+        return c4_p403_1;
+    }
+
+    public void setC4_p403_1(String c4_p403_1) {
+        this.c4_p403_1 = c4_p403_1;
+    }
+
+    public String getC4_p403_2() {
+        return c4_p403_2;
+    }
+
+    public void setC4_p403_2(String c4_p403_2) {
+        this.c4_p403_2 = c4_p403_2;
+    }
+
+    public String getC4_p403_3() {
+        return c4_p403_3;
+    }
+
+    public void setC4_p403_3(String c4_p403_3) {
+        this.c4_p403_3 = c4_p403_3;
+    }
+
+    public String getC4_p403_4() {
+        return c4_p403_4;
+    }
+
+    public void setC4_p403_4(String c4_p403_4) {
+        this.c4_p403_4 = c4_p403_4;
+    }
+
+    public String getC4_p403_5() {
+        return c4_p403_5;
+    }
+
+    public void setC4_p403_5(String c4_p403_5) {
+        this.c4_p403_5 = c4_p403_5;
+    }
+
+    public String getC4_p403_6() {
+        return c4_p403_6;
+    }
+
+    public void setC4_p403_6(String c4_p403_6) {
+        this.c4_p403_6 = c4_p403_6;
+    }
+
+    public String getC4_p403_7() {
+        return c4_p403_7;
+    }
+
+    public void setC4_p403_7(String c4_p403_7) {
+        this.c4_p403_7 = c4_p403_7;
+    }
+
+    public String getC4_p403_8() {
+        return c4_p403_8;
+    }
+
+    public void setC4_p403_8(String c4_p403_8) {
+        this.c4_p403_8 = c4_p403_8;
+    }
+
+    public String getC4_p403_9() {
+        return c4_p403_9;
+    }
+
+    public void setC4_p403_9(String c4_p403_9) {
+        this.c4_p403_9 = c4_p403_9;
+    }
+
+    public String getC4_p403_10() {
+        return c4_p403_10;
+    }
+
+    public void setC4_p403_10(String c4_p403_10) {
+        this.c4_p403_10 = c4_p403_10;
+    }
+
+    public String getC4_p403_11() {
+        return c4_p403_11;
+    }
+
+    public void setC4_p403_11(String c4_p403_11) {
+        this.c4_p403_11 = c4_p403_11;
+    }
+
+    public String getC4_p403_12() {
+        return c4_p403_12;
+    }
+
+    public void setC4_p403_12(String c4_p403_12) {
+        this.c4_p403_12 = c4_p403_12;
+    }
+
+    public String getC4_p403_13() {
+        return c4_p403_13;
+    }
+
+    public void setC4_p403_13(String c4_p403_13) {
+        this.c4_p403_13 = c4_p403_13;
+    }
+
+    public String getC4_p403_14() {
+        return c4_p403_14;
+    }
+
+    public void setC4_p403_14(String c4_p403_14) {
+        this.c4_p403_14 = c4_p403_14;
+    }
+
+    public String getC4_p403_o() {
+        return c4_p403_o;
+    }
+
+    public void setC4_p403_o(String c4_p403_o) {
+        this.c4_p403_o = c4_p403_o;
+    }
+
+    public String getC4_p404() {
+        return c4_p404;
+    }
+
+    public void setC4_p404(String c4_p404) {
+        this.c4_p404 = c4_p404;
+    }
+
+    public String getC4_p405_1() {
+        return c4_p405_1;
+    }
+
+    public void setC4_p405_1(String c4_p405_1) {
+        this.c4_p405_1 = c4_p405_1;
+    }
+
+    public String getC4_p405_2() {
+        return c4_p405_2;
+    }
+
+    public void setC4_p405_2(String c4_p405_2) {
+        this.c4_p405_2 = c4_p405_2;
+    }
+
+    public String getC4_p405_3() {
+        return c4_p405_3;
+    }
+
+    public void setC4_p405_3(String c4_p405_3) {
+        this.c4_p405_3 = c4_p405_3;
+    }
+
+    public String getC4_p405_4() {
+        return c4_p405_4;
+    }
+
+    public void setC4_p405_4(String c4_p405_4) {
+        this.c4_p405_4 = c4_p405_4;
+    }
+
+    public String getC4_p405_5() {
+        return c4_p405_5;
+    }
+
+    public void setC4_p405_5(String c4_p405_5) {
+        this.c4_p405_5 = c4_p405_5;
+    }
+
+    public String getC4_p405_6() {
+        return c4_p405_6;
+    }
+
+    public void setC4_p405_6(String c4_p405_6) {
+        this.c4_p405_6 = c4_p405_6;
+    }
+
+    public String getC4_p405_7() {
+        return c4_p405_7;
+    }
+
+    public void setC4_p405_7(String c4_p405_7) {
+        this.c4_p405_7 = c4_p405_7;
+    }
+
+    public String getC4_p406_1() {
+        return c4_p406_1;
+    }
+
+    public void setC4_p406_1(String c4_p406_1) {
+        this.c4_p406_1 = c4_p406_1;
+    }
+
+    public String getC4_p406_2() {
+        return c4_p406_2;
+    }
+
+    public void setC4_p406_2(String c4_p406_2) {
+        this.c4_p406_2 = c4_p406_2;
+    }
+
+    public String getC4_p406_3() {
+        return c4_p406_3;
+    }
+
+    public void setC4_p406_3(String c4_p406_3) {
+        this.c4_p406_3 = c4_p406_3;
+    }
+
+    public String getC4_p406_4() {
+        return c4_p406_4;
+    }
+
+    public void setC4_p406_4(String c4_p406_4) {
+        this.c4_p406_4 = c4_p406_4;
+    }
+
+    public String getC4_p406_5() {
+        return c4_p406_5;
+    }
+
+    public void setC4_p406_5(String c4_p406_5) {
+        this.c4_p406_5 = c4_p406_5;
+    }
+
+    public String getC4_p406_6() {
+        return c4_p406_6;
+    }
+
+    public void setC4_p406_6(String c4_p406_6) {
+        this.c4_p406_6 = c4_p406_6;
+    }
+
+    public String getC4_p406_7() {
+        return c4_p406_7;
+    }
+
+    public void setC4_p406_7(String c4_p406_7) {
+        this.c4_p406_7 = c4_p406_7;
+    }
+
+    public String getC4_p406_8() {
+        return c4_p406_8;
+    }
+
+    public void setC4_p406_8(String c4_p406_8) {
+        this.c4_p406_8 = c4_p406_8;
+    }
+
+    public String getC4_p406_o() {
+        return c4_p406_o;
+    }
+
+    public void setC4_p406_o(String c4_p406_o) {
+        this.c4_p406_o = c4_p406_o;
+    }
+
+    public String getC4_p407_1() {
+        return c4_p407_1;
+    }
+
+    public void setC4_p407_1(String c4_p407_1) {
+        this.c4_p407_1 = c4_p407_1;
+    }
+
+    public String getC4_p407_2() {
+        return c4_p407_2;
+    }
+
+    public void setC4_p407_2(String c4_p407_2) {
+        this.c4_p407_2 = c4_p407_2;
+    }
+
+    public String getC4_p407_3() {
+        return c4_p407_3;
+    }
+
+    public void setC4_p407_3(String c4_p407_3) {
+        this.c4_p407_3 = c4_p407_3;
+    }
+
+    public String getC4_p407_4() {
+        return c4_p407_4;
+    }
+
+    public void setC4_p407_4(String c4_p407_4) {
+        this.c4_p407_4 = c4_p407_4;
+    }
+
+    public String getC4_p407_5() {
+        return c4_p407_5;
+    }
+
+    public void setC4_p407_5(String c4_p407_5) {
+        this.c4_p407_5 = c4_p407_5;
+    }
+
+    public String getC4_p407_6() {
+        return c4_p407_6;
+    }
+
+    public void setC4_p407_6(String c4_p407_6) {
+        this.c4_p407_6 = c4_p407_6;
+    }
+
+    public String getC4_p407_7() {
+        return c4_p407_7;
+    }
+
+    public void setC4_p407_7(String c4_p407_7) {
+        this.c4_p407_7 = c4_p407_7;
+    }
+
+    public String getC4_p407_8() {
+        return c4_p407_8;
+    }
+
+    public void setC4_p407_8(String c4_p407_8) {
+        this.c4_p407_8 = c4_p407_8;
+    }
+
+    public String getC4_p407_9() {
+        return c4_p407_9;
+    }
+
+    public void setC4_p407_9(String c4_p407_9) {
+        this.c4_p407_9 = c4_p407_9;
+    }
+
+    public String getC4_p407_10() {
+        return c4_p407_10;
+    }
+
+    public void setC4_p407_10(String c4_p407_10) {
+        this.c4_p407_10 = c4_p407_10;
+    }
+
+    public String getC4_p407_11() {
+        return c4_p407_11;
+    }
+
+    public void setC4_p407_11(String c4_p407_11) {
+        this.c4_p407_11 = c4_p407_11;
+    }
+
+    public String getC4_p407_12() {
+        return c4_p407_12;
+    }
+
+    public void setC4_p407_12(String c4_p407_12) {
+        this.c4_p407_12 = c4_p407_12;
+    }
+
+    public String getC4_p407_13() {
+        return c4_p407_13;
+    }
+
+    public void setC4_p407_13(String c4_p407_13) {
+        this.c4_p407_13 = c4_p407_13;
+    }
+
+    public String getC4_p407_o() {
+        return c4_p407_o;
+    }
+
+    public void setC4_p407_o(String c4_p407_o) {
+        this.c4_p407_o = c4_p407_o;
+    }
+
+    public String getC4_p408_1() {
+        return c4_p408_1;
+    }
+
+    public void setC4_p408_1(String c4_p408_1) {
+        this.c4_p408_1 = c4_p408_1;
+    }
+
+    public String getC4_p408_2() {
+        return c4_p408_2;
+    }
+
+    public void setC4_p408_2(String c4_p408_2) {
+        this.c4_p408_2 = c4_p408_2;
+    }
+
+    public String getC4_p408_3() {
+        return c4_p408_3;
+    }
+
+    public void setC4_p408_3(String c4_p408_3) {
+        this.c4_p408_3 = c4_p408_3;
+    }
+
+    public String getC4_p408_4() {
+        return c4_p408_4;
+    }
+
+    public void setC4_p408_4(String c4_p408_4) {
+        this.c4_p408_4 = c4_p408_4;
+    }
+
+    public String getC4_p408_5() {
+        return c4_p408_5;
+    }
+
+    public void setC4_p408_5(String c4_p408_5) {
+        this.c4_p408_5 = c4_p408_5;
+    }
+
+    public String getC4_p408_6() {
+        return c4_p408_6;
+    }
+
+    public void setC4_p408_6(String c4_p408_6) {
+        this.c4_p408_6 = c4_p408_6;
+    }
+
+    public String getC4_p408_7() {
+        return c4_p408_7;
+    }
+
+    public void setC4_p408_7(String c4_p408_7) {
+        this.c4_p408_7 = c4_p408_7;
+    }
+
+    public String getC4_p408_8() {
+        return c4_p408_8;
+    }
+
+    public void setC4_p408_8(String c4_p408_8) {
+        this.c4_p408_8 = c4_p408_8;
+    }
+
+    public String getC4_p408_9() {
+        return c4_p408_9;
+    }
+
+    public void setC4_p408_9(String c4_p408_9) {
+        this.c4_p408_9 = c4_p408_9;
+    }
+
+    public String getC4_p408_10() {
+        return c4_p408_10;
+    }
+
+    public void setC4_p408_10(String c4_p408_10) {
+        this.c4_p408_10 = c4_p408_10;
+    }
+
+    public String getC4_p408_11() {
+        return c4_p408_11;
+    }
+
+    public void setC4_p408_11(String c4_p408_11) {
+        this.c4_p408_11 = c4_p408_11;
+    }
+
+    public String getC4_p408_12() {
+        return c4_p408_12;
+    }
+
+    public void setC4_p408_12(String c4_p408_12) {
+        this.c4_p408_12 = c4_p408_12;
+    }
+
+    public String getC4_p408_13() {
+        return c4_p408_13;
+    }
+
+    public void setC4_p408_13(String c4_p408_13) {
+        this.c4_p408_13 = c4_p408_13;
+    }
+
+    public String getC4_p408_14() {
+        return c4_p408_14;
+    }
+
+    public void setC4_p408_14(String c4_p408_14) {
+        this.c4_p408_14 = c4_p408_14;
+    }
+
+    public String getC4_p408_o() {
+        return c4_p408_o;
+    }
+
+    public void setC4_p408_o(String c4_p408_o) {
+        this.c4_p408_o = c4_p408_o;
+    }
+
+    public String getC4_p409() {
+        return c4_p409;
+    }
+
+    public void setC4_p409(String c4_p409) {
+        this.c4_p409 = c4_p409;
+    }
+
+    public String getC4_p410_1() {
+        return c4_p410_1;
+    }
+
+    public void setC4_p410_1(String c4_p410_1) {
+        this.c4_p410_1 = c4_p410_1;
+    }
+
+    public String getC4_p410_2() {
+        return c4_p410_2;
+    }
+
+    public void setC4_p410_2(String c4_p410_2) {
+        this.c4_p410_2 = c4_p410_2;
+    }
+
+    public String getC4_p410_3() {
+        return c4_p410_3;
+    }
+
+    public void setC4_p410_3(String c4_p410_3) {
+        this.c4_p410_3 = c4_p410_3;
+    }
+
+    public String getC4_p410_4() {
+        return c4_p410_4;
+    }
+
+    public void setC4_p410_4(String c4_p410_4) {
+        this.c4_p410_4 = c4_p410_4;
+    }
+
+    public String getC4_p411() {
+        return c4_p411;
+    }
+
+    public void setC4_p411(String c4_p411) {
+        this.c4_p411 = c4_p411;
+    }
+
+    public String getC4_p412() {
+        return c4_p412;
+    }
+
+    public void setC4_p412(String c4_p412) {
+        this.c4_p412 = c4_p412;
+    }
+
+    public String getC4_p413() {
+        return c4_p413;
+    }
+
+    public void setC4_p413(String c4_p413) {
+        this.c4_p413 = c4_p413;
+    }
+
+    public String getC4_p414() {
+        return c4_p414;
+    }
+
+    public void setC4_p414(String c4_p414) {
+        this.c4_p414 = c4_p414;
+    }
+
+    public String getC4_p415_1() {
+        return c4_p415_1;
+    }
+
+    public void setC4_p415_1(String c4_p415_1) {
+        this.c4_p415_1 = c4_p415_1;
+    }
+
+    public String getC4_p415_2() {
+        return c4_p415_2;
+    }
+
+    public void setC4_p415_2(String c4_p415_2) {
+        this.c4_p415_2 = c4_p415_2;
+    }
+
+    public String getC4_p415_3() {
+        return c4_p415_3;
+    }
+
+    public void setC4_p415_3(String c4_p415_3) {
+        this.c4_p415_3 = c4_p415_3;
+    }
+
+    public String getC4_p415_4() {
+        return c4_p415_4;
+    }
+
+    public void setC4_p415_4(String c4_p415_4) {
+        this.c4_p415_4 = c4_p415_4;
+    }
+
+    public String getC4_p415_5() {
+        return c4_p415_5;
+    }
+
+    public void setC4_p415_5(String c4_p415_5) {
+        this.c4_p415_5 = c4_p415_5;
+    }
+
+    public String getC4_p415_6() {
+        return c4_p415_6;
+    }
+
+    public void setC4_p415_6(String c4_p415_6) {
+        this.c4_p415_6 = c4_p415_6;
+    }
+
+    public String getC4_p415_7() {
+        return c4_p415_7;
+    }
+
+    public void setC4_p415_7(String c4_p415_7) {
+        this.c4_p415_7 = c4_p415_7;
+    }
+
+    public String getC4_p415_8() {
+        return c4_p415_8;
+    }
+
+    public void setC4_p415_8(String c4_p415_8) {
+        this.c4_p415_8 = c4_p415_8;
+    }
+
+    public String getC4_p415_o() {
+        return c4_p415_o;
+    }
+
+    public void setC4_p415_o(String c4_p415_o) {
+        this.c4_p415_o = c4_p415_o;
+    }
+
+    public String getC4_p416_1() {
+        return c4_p416_1;
+    }
+
+    public void setC4_p416_1(String c4_p416_1) {
+        this.c4_p416_1 = c4_p416_1;
+    }
+
+    public String getC4_p416_2() {
+        return c4_p416_2;
+    }
+
+    public void setC4_p416_2(String c4_p416_2) {
+        this.c4_p416_2 = c4_p416_2;
+    }
+
+    public String getC4_p416_3() {
+        return c4_p416_3;
+    }
+
+    public void setC4_p416_3(String c4_p416_3) {
+        this.c4_p416_3 = c4_p416_3;
+    }
+
+    public String getC4_p416_4() {
+        return c4_p416_4;
+    }
+
+    public void setC4_p416_4(String c4_p416_4) {
+        this.c4_p416_4 = c4_p416_4;
+    }
+
+    public String getC4_p416_5() {
+        return c4_p416_5;
+    }
+
+    public void setC4_p416_5(String c4_p416_5) {
+        this.c4_p416_5 = c4_p416_5;
+    }
+
+    public String getC4_p416_6() {
+        return c4_p416_6;
+    }
+
+    public void setC4_p416_6(String c4_p416_6) {
+        this.c4_p416_6 = c4_p416_6;
+    }
+
+    public String getObs_cap4() {
+        return obs_cap4;
+    }
+
+    public void setObs_cap4(String obs_cap4) {
+        this.obs_cap4 = obs_cap4;
+    }
+
+    public String getC4_estado() {
+        return c4_estado;
+    }
+
+    public void setC4_estado(String c4_estado) {
+        this.c4_estado = c4_estado;
+    }
+
     private String c4_p408_9;
     private String c4_p408_10;
     private String c4_p408_11;
--- app/src/main/java/com/example/ricindigus/empove2018/modelo/pojos/Modulo5.java
+++ app/src/main/java/com/example/ricindigus/empove2018/modelo/pojos/Modulo5.java
@@ -46,6 +46,326 @@ public class Modulo5 {
     private String obs_cap5;
     private String c5_estado;
 
+    public int get_id() {
+        return _id;
+    }
+
+    public void set_id(int _id) {
+        this._id = _id;
+    }
+
+    public String getIdInformante() {
+        return idInformante;
+    }
+
+    public void setIdInformante(String idInformante) {
+        this.idInformante = idInformante;
+    }
+
+    public String getIdHogar() {
+        return idHogar;
+    }
+
+    public void setIdHogar(String idHogar) {
+        this.idHogar = idHogar;
+    }
+
+    public String getIdVivienda() {
+        return idVivienda;
+    }
+
+    public void setIdVivienda(String idVivienda) {
+        this.idVivienda = idVivienda;
+    }
+
+    public String getC5_p501() {
+        return c5_p501;
+    }
+
+    public void setC5_p501(String c5_p501) {
+        this.c5_p501 = c5_p501;
+    }
+
+    public String getC5_p502_c() {
+        return c5_p502_c;
+    }
+
+    public void setC5_p502_c(String c5_p502_c) {
+        this.c5_p502_c = c5_p502_c;
+    }
+
+    public String getC5_p502() {
+        return c5_p502;
+    }
+
+    public void setC5_p502(String c5_p502) {
+        this.c5_p502 = c5_p502;
+    }
+
+    public String getC5_p503() {
+        return c5_p503;
+    }
+
+    public void setC5_p503(String c5_p503) {
+        this.c5_p503 = c5_p503;
+    }
+
+    public String getC5_p504() {
+        return c5_p504;
+    }
+
+    public void setC5_p504(String c5_p504) {
+        this.c5_p504 = c5_p504;
+    }
+
+    public String getC5_p505() {
+        return c5_p505;
+    }
+
+    public void setC5_p505(String c5_p505) {
+        this.c5_p505 = c5_p505;
+    }
+
+    public String getC5_p506_1() {
+        return c5_p506_1;
+    }
+
+    public void setC5_p506_1(String c5_p506_1) {
+        this.c5_p506_1 = c5_p506_1;
+    }
+
+    public String getC5_p506_2() {
+        return c5_p506_2;
+    }
+
+    public void setC5_p506_2(String c5_p506_2) {
+        this.c5_p506_2 = c5_p506_2;
+    }
+
+    public String getC5_p506_3() {
+        return c5_p506_3;
+    }
+
+    public void setC5_p506_3(String c5_p506_3) {
+        this.c5_p506_3 = c5_p506_3;
+    }
+
+    public String getC5_p506_4() {
+        return c5_p506_4;
+    }
+
+    public void setC5_p506_4(String c5_p506_4) {
+        this.c5_p506_4 = c5_p506_4;
+    }
+
+    public String getC5_p507() {
+        return c5_p507;
+    }
+
+    public void setC5_p507(String c5_p507) {
+        this.c5_p507 = c5_p507;
+    }
+
+    public String getC5_p507_dist() {
+        return c5_p507_dist;
+    }
+
+    public void setC5_p507_dist(String c5_p507_dist) {
+        this.c5_p507_dist = c5_p507_dist;
+    }
+
+    public String getC5_p507_prov() {
+        return c5_p507_prov;
+    }
+
+    public void setC5_p507_prov(String c5_p507_prov) {
+        this.c5_p507_prov = c5_p507_prov;
+    }
+
+    public String getC5_p507_dep() {
+        return c5_p507_dep;
+    }
+
+    public void setC5_p507_dep(String c5_p507_dep) {
+        this.c5_p507_dep = c5_p507_dep;
+    }
+
+    public String getC5_p508_1() {
+        return c5_p508_1;
+    }
+
+    public void setC5_p508_1(String c5_p508_1) {
+        this.c5_p508_1 = c5_p508_1;
+    }
+
+    public String getC5_p508_2() {
+        return c5_p508_2;
+    }
+
+    public void setC5_p508_2(String c5_p508_2) {
+        this.c5_p508_2 = c5_p508_2;
+    }
+
+    public String getC5_p508_3() {
+        return c5_p508_3;
+    }
+
+    public void setC5_p508_3(String c5_p508_3) {
+        this.c5_p508_3 = c5_p508_3;
+    }
+
+    public String getC5_p508_4() {
+        return c5_p508_4;
+    }
+
+    public void setC5_p508_4(String c5_p508_4) {
+        this.c5_p508_4 = c5_p508_4;
+    }
+
+    public String getC5_p508_5() {
+        return c5_p508_5;
+    }
+
+    public void setC5_p508_5(String c5_p508_5) {
+        this.c5_p508_5 = c5_p508_5;
+    }
+
+    public String getC5_p508_6() {
+        return c5_p508_6;
+    }
+
+    public void setC5_p508_6(String c5_p508_6) {
+        this.c5_p508_6 = c5_p508_6;
+    }
+
+    public String getC5_p508_7() {
+        return c5_p508_7;
+    }
+
+    public void setC5_p508_7(String c5_p508_7) {
+        this.c5_p508_7 = c5_p508_7;
+    }
+
+    public String getC5_p508_8() {
+        return c5_p508_8;
+    }
+
+    public void setC5_p508_8(String c5_p508_8) {
+        this.c5_p508_8 = c5_p508_8;
+    }
+
+    public String getC5_p508_9() {
+        return c5_p508_9;
+    }
+
+    public void setC5_p508_9(String c5_p508_9) {
+        this.c5_p508_9 = c5_p508_9;
+    }
+
+    public String getC5_p508_10() {
+        return c5_p508_10;
+    }
+
+    public void setC5_p508_10(String c5_p508_10) {
+        this.c5_p508_10 = c5_p508_10;
+    }
+
+    public String getC5_p508_11() {
+        return c5_p508_11;
+    }
+
+    public void setC5_p508_11(String c5_p508_11) {
+        this.c5_p508_11 = c5_p508_11;
+    }
+
+    public String getC5_p508_o() {
+        return c5_p508_o;
+    }
+
+    public void setC5_p508_o(String c5_p508_o) {
+        this.c5_p508_o = c5_p508_o;
+    }
+
+    public String getC5_p509() {
+        return c5_p509;
+    }
+
+    public void setC5_p509(String c5_p509) {
+        this.c5_p509 = c5_p509;
+    }
+
+    public String getC5_p510() {
+        return c5_p510;
+    }
+
+    public void setC5_p510(String c5_p510) {
+        this.c5_p510 = c5_p510;
+    }
+
+    public String getC5_p511() {
+        return c5_p511;
+    }
+
+    public void setC5_p511(String c5_p511) {
+        this.c5_p511 = c5_p511;
+    }
+
+    public String getC5_p511_o() {
+        return c5_p511_o;
+    }
+
+    public void setC5_p511_o(String c5_p511_o) {
+        this.c5_p511_o = c5_p511_o;
+    }
+
+    public String getC5_p512() {
+        return c5_p512;
+    }
+
+    public void setC5_p512(String c5_p512) {
+        this.c5_p512 = c5_p512;
+    }
+
+    public String getC5_p512_o() {
+        return c5_p512_o;
+    }
+
+    public void setC5_p512_o(String c5_p512_o) {
+        this.c5_p512_o = c5_p512_o;
+    }
+
+    public String getC5_p513() {
+        return c5_p513;
+    }
+
+    public void setC5_p513(String c5_p513) {
+        this.c5_p513 = c5_p513;
+    }
+
+    public String getC5_p513_o() {
+        return c5_p513_o;
+    }
+
+    public void setC5_p513_o(String c5_p513_o) {
+        this.c5_p513_o = c5_p513_o;
+    }
+
+    public String getObs_cap5() {
+        return obs_cap5;
+    }
+
+    public void setObs_cap5(String obs_cap5) {
+        this.obs_cap5 = obs_cap5;
+    }
+
+    public String getC5_estado() {
+        return c5_estado;
+    }
+
+    public void setC5_estado(String c5_estado) {
+        this.c5_estado = c5_estado;
+    }
+
     public ContentValues toValues(){
         ContentValues contentValues = new ContentValues();
         contentValues.put(SQLConstantes.modulo3_id,_id);
@@ -68,6 +388,7 @@ public class Modulo5 {
         contentValues.put(SQLConstantes.modulo5_c5_p507_dep,c5_p507_dep);
         contentValues.put(SQLConstantes.modulo5_c5_p508_1,c5_p508_1);
         contentValues.put(SQLConstantes.modulo5_c5_p508_2,c5_p508_2);
+
         contentValues.put(SQLConstantes.modulo5_c5_p508_3,c5_p508_3);
         contentValues.put(SQLConstantes.modulo5_c5_p508_4,c5_p508_4);
         contentValues.put(SQLConstantes.modulo5_c5_p508_5,c5_p508_5);
