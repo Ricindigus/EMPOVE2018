0678dd6feed1687b0f80d0b6c5e1ab2b5e1c8f55
Leshem Hilario Salazar <leshem.hilario@inei.gob.pe>
2018-10-19 16:41:55 -0500

    DatePicker 301 y 307

--- app/src/main/java/com/example/ricindigus/empove2018/fragments/modulo3/FragmentP301P305.java
+++ app/src/main/java/com/example/ricindigus/empove2018/fragments/modulo3/FragmentP301P305.java
@@ -1,19 +1,35 @@
 package com.example.ricindigus.empove2018.fragments.modulo3;
 
 
+import android.app.DatePickerDialog;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
 
 import com.example.ricindigus.empove2018.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class FragmentP301P305 extends Fragment {
 
+import java.util.Calendar;
+
+public class FragmentP301P305 extends Fragment {
+    Context context;
+    private static final String CERO = "0";
+    public final Calendar c = Calendar.getInstance();
+    final int mes = c.get(Calendar.MONTH);
+    final int dia = c.get(Calendar.DAY_OF_MONTH);
+    final int anio = c.get(Calendar.YEAR);
+
+    TextView tv_fecha_dia, tv_fecha_mes, tv_fecha_anio;
+    Button btn_obtenerFecha;
 
     public FragmentP301P305() {
         // Required empty public constructor
@@ -26,9 +42,69 @@ public class FragmentP301P305 extends Fragment {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_p301_p305, container, false);
 
+        tv_fecha_dia = (TextView) rootView.findViewById(R.id.mostrar_fecha_dia_TextView);
+        tv_fecha_mes = (TextView) rootView.findViewById(R.id.mostrar_fecha_mes_TextView);
+        tv_fecha_anio = (TextView) rootView.findViewById(R.id.mostrar_fecha_anio_TextView);
+        btn_obtenerFecha = (Button) rootView.findViewById(R.id.btn_obtener_fecha);
+        btn_obtenerFecha.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+                DatePickerDialog recogerFecha = new DatePickerDialog(getContext(), new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        final int mesActual = month + 1;
+                        String diaFormateado = (dayOfMonth < 10)? CERO + String.valueOf(dayOfMonth):String.valueOf(dayOfMonth);
+                        String mesFormateado = (mesActual < 10)? CERO + String.valueOf(mesActual):String.valueOf(mesActual);
+                        tv_fecha_dia.setText(""+diaFormateado);
+                        tv_fecha_mes.setText(""+mesFormateado);
+                        tv_fecha_anio.setText(""+year);
+
+                    }
+                    //Estos valores deben ir en ese orden, de lo contrario no mostrara la fecha actual
+                    /**
+                     *También puede cargar los valores que usted desee
+                     */
+                },anio, mes, dia);
+                //Muestro el widget
+                recogerFecha.show();
+            }
+        });
 
         return rootView;
     }
 
+//    @Override
+//    public void onClick(View v) {
+//        switch (v.getId()){
+//            case R.id.btn_obtener_fecha:
+//                obtenerFecha();
+//                break;
+//        }
+//    }
+//
+//    private void obtenerFecha() {
+//        DatePickerDialog recogerFecha = new DatePickerDialog(context, new DatePickerDialog.OnDateSetListener() {
+//            @Override
+//            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+//                //Esta variable lo que realiza es aumentar en uno el mes ya que comienza desde 0 = enero
+//                final int mesActual = month + 1;
+//                //Formateo el día obtenido: antepone el 0 si son menores de 10
+//                String diaFormateado = (dayOfMonth < 10)? CERO + String.valueOf(dayOfMonth):String.valueOf(dayOfMonth);
+//                //Formateo el mes obtenido: antepone el 0 si son menores de 10
+//                String mesFormateado = (mesActual < 10)? CERO + String.valueOf(mesActual):String.valueOf(mesActual);
+//                //Muestro la fecha con el formato deseado
+//                tv_fecha_dia.setText(""+diaFormateado);
+//                //tv_fecha_mes.setText(""+mesFormateado);
+//                //tv_fecha_anio.setText(""+year);
+//
+//            }
+//            //Estos valores deben ir en ese orden, de lo contrario no mostrara la fecha actual
+//            /**
+//             *También puede cargar los valores que usted desee
+//             */
+//        },anio, mes, dia);
+//        //Muestro el widget
+//        recogerFecha.show();
+//    }
 }
--- app/src/main/java/com/example/ricindigus/empove2018/fragments/modulo3/FragmentP306P308.java
+++ app/src/main/java/com/example/ricindigus/empove2018/fragments/modulo3/FragmentP306P308.java
@@ -1,20 +1,34 @@
 package com.example.ricindigus.empove2018.fragments.modulo3;
 
 
+import android.app.DatePickerDialog;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
 
 import com.example.ricindigus.empove2018.R;
+
+import java.util.Calendar;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class FragmentP306P308 extends Fragment {
+    Context context;
+    private static final String CERO = "0";
+    public final Calendar c = Calendar.getInstance();
+    final int mes = c.get(Calendar.MONTH);
+    final int dia = c.get(Calendar.DAY_OF_MONTH);
+    final int anio = c.get(Calendar.YEAR);
 
-
+    TextView tv_fecha_dia_307, tv_fecha_mes_307, tv_fecha_anio_307;
+    Button btn_obtenerFecha_307;
     public FragmentP306P308() {
         // Required empty public constructor
     }
@@ -24,7 +38,32 @@ public class FragmentP306P308 extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_p306_p308, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_p306_p308, container, false);
+        tv_fecha_dia_307 = (TextView) rootView.findViewById(R.id.mostrar_fecha_dia_307_TextView);
+        tv_fecha_mes_307 = (TextView) rootView.findViewById(R.id.mostrar_fecha_mes_307_TextView);
+        tv_fecha_anio_307 = (TextView) rootView.findViewById(R.id.mostrar_fecha_anio_307_TextView);
+        btn_obtenerFecha_307 = (Button) rootView.findViewById(R.id.btn_obtener_fecha_307);
+
+        btn_obtenerFecha_307.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                DatePickerDialog recogeFecha = new DatePickerDialog(getContext(), new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        final int mesActual = month + 1;
+                        String diaFormateado = (dayOfMonth < 10)? CERO + String.valueOf(dayOfMonth):String.valueOf(dayOfMonth);
+                        String mesFormateado = (mesActual < 10)? CERO + String.valueOf(mesActual):String.valueOf(mesActual);
+                        tv_fecha_dia_307.setText(""+diaFormateado);
+                        tv_fecha_mes_307.setText(""+mesFormateado);
+                        tv_fecha_anio_307.setText(""+year);
+                    }
+                },anio,mes,dia);
+                recogeFecha.show();
+            }
+        });
+
+        return rootView;
     }
 
 }
